f1d75db33d48a898d13232d1d5541a15e4cba868
Devarajan Nachimuthu <ndevaraj424@gmail.com>
2018-08-15 18:17:35 +0530

    App prepared for Heroku
    Some nodejs chnages made for Heroku

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const PORT = process.env.PORT || 5000
 
 var app = express();
 
-var staticPath = path.join(__dirname, '/public');
+var staticPath = path.join(__dirname, '');
 app.use(express.static(staticPath));
-
+app.get('/', function (req, res) {  
+  res.redirect('/');  
+}); 
 app.listen(PORT, function() {
   console.log('listening on '+PORT);
 });
